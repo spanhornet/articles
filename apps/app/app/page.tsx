13494edab743cc232d74a5852e412499e2b868a1
Ayush Kumar <ayushkumar@Ayushs-MacBook-Air.local>
2025-03-25 17:51:22 -0500

    feat(next-themes): implement dark mode

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -1,11 +1,11 @@
 // Shadcn UI
-import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
    <>
     <div>
-      <Button>Click me</Button>
+      <ModeToggle />
     </div>
    </>
   );
